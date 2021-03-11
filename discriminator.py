dc3f20bc67536f72bb56c6c0711ffe14f79e4f4a
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2021-03-11 16:01:02 +0530

    lil changes

--- discriminator.py
+++ discriminator.py
@@ -8,23 +8,23 @@ class Discriminator(torch.nn.Module):
             # input is (input_channel) x 128 x 128
             torch.nn.Conv2d(input_channel, discriminator_feature_map_size, 4, 2, 1, bias=False),
             torch.nn.LeakyReLU(0.2, inplace=True),
-            # state size. (discriminator_feature_map_size*2) x 64 x 64
+            # state size. (discriminator_feature_map_size) x 64 x 64
             torch.nn.Conv2d(discriminator_feature_map_size, discriminator_feature_map_size * 2, 4, 2, 1, bias=False),
+            torch.nn.BatchNorm2d(discriminator_feature_map_size * 2),
+            torch.nn.LeakyReLU(0.2, inplace=True),
+            # state size. (discriminator_feature_map_size*2) x 32 x 32
+            torch.nn.Conv2d(discriminator_feature_map_size * 2, discriminator_feature_map_size * 4, 4, 2, 1, bias=False),
             torch.nn.BatchNorm2d(discriminator_feature_map_size * 4),
             torch.nn.LeakyReLU(0.2, inplace=True),
-            # state size. (discriminator_feature_map_size*4) x 32 x 32
-            torch.nn.Conv2d(discriminator_feature_map_size * 2, discriminator_feature_map_size * 4, 4, 2, 1, bias=False),
-            torch.nn.BatchNorm2d(discriminator_feature_map_size * 8),
-            torch.nn.LeakyReLU(0.2, inplace=True),
-            # state size. (discriminator_feature_map_size*8) x 16 x 16
+            # state size. (discriminator_feature_map_size*4) x 16 x 16
             torch.nn.Conv2d(discriminator_feature_map_size * 4, discriminator_feature_map_size * 8, 4, 2, 1, bias=False),
             torch.nn.BatchNorm2d(discriminator_feature_map_size * 8),
             torch.nn.LeakyReLU(0.2, inplace=True),
-            # state size. (discriminator_feature_map_size*16) x 8 x 8
+            # state size. (discriminator_feature_map_size*8) x 8 x 8
             torch.nn.Conv2d(discriminator_feature_map_size * 8, discriminator_feature_map_size * 16, 4, 2, 1, bias=False),
-            torch.nn.BatchNorm2d(discriminator_feature_map_size * 8),
+            torch.nn.BatchNorm2d(discriminator_feature_map_size * 16),
             torch.nn.LeakyReLU(0.2, inplace=True),            
-            # state size. (discriminator_feature_map_size*32) x 4 x 4
+            # state size. (discriminator_feature_map_size*16) x 4 x 4
             torch.nn.Conv2d(discriminator_feature_map_size * 16, 1, 4, 1, 0, bias=False),
             torch.nn.Sigmoid()
         )
